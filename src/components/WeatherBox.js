8e0f2ad12049b20a03302b9c88e8555196ff9538
bitna kim <tallshe108@hanmail.net>
2023-08-01 10:56:12 +0700

    show fail message

--- src/components/WeatherBox.js
+++ src/components/WeatherBox.js
@@ -8,11 +8,12 @@ const WeatherBox = ({ weather }) => {
     weather && weather.main
       ? (((weather.main.temp - 273.15) * 9) / 5 + 32).toFixed(2)
       : "";
+
   return (
     <Card className="weather-card">
       {/* <Card.Img src="holder.js/100px270" alt="Card image" /> */}
       <Card.ImgOverlay className="d-flex flex-column justify-content-center text-center">
-        <Card.Title>{weather?.name}</Card.Title>
+        <Card.Title>{weather?.name || '날씨를 가져오는데 실패했습니다.'}</Card.Title>
         <Card.Text className="text-success h1">
           {`${temperatureC} °C / ${temperatureF} °F`}
         </Card.Text>
